6088e2777604aea5b1cc910386dcf3ef2099e787
Ryan Fanchiotti <112440089+ryanfanchiotti@users.noreply.github.com>
2022-09-14 21:38:47 +0000

    guess the number p4

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -1,19 +1,19 @@
 import random
 
 number = random.randint(1, 10)
-
+print(number)
 guess = int(input("Guess a number between 1 and 10"))
 for i in range(0,2):
-  if number == guess:
-    print("Correct")
-    exit()
   if number < guess: 
     print("Too high")
     guess = int(input("Guess again"))
-  if number > guess:
+  elif number > guess:
     print("Too low")
     guess = int(input("Guess again"))
-if number == guess:
+  else:
     print("Correct")
     exit()
+if number == guess:
+  print("Correct")
+  exit()
 print("Incorrect, the correct number was", number)

bf5b15b5bf474ff2c82184aa0ca126a91b357cb5
Ryan Fanchiotti <112440089+ryanfanchiotti@users.noreply.github.com>
2022-09-14 21:21:17 +0000

    guess the number

--- README.md
+++ README.md
@@ -14,4 +14,4 @@ Preferred Name: Ryan
 
 Discord username: Ryan Fanchiotti#9387
 
-REPL url: https://replit.com/@RyanFanchiotti1
+REPL url: [https://replit.com/@RyanFanchiotti1](https://replit.com/join/vwndawwwip-ryanfanchiotti1)
---
+++ ch03/exercises/guess.py
@@ -0,0 +1,16 @@
+import random
+
+number = random.randint(1, 10)
+
+guess = int(input("Guess a number between 1 and 10"))
+for i in range(0,2):
+  if number == guess:
+    print("You guessed it")
+    exit()
+  if number < guess: 
+    print("Too high")
+    guess = int(input("Guess again"))
+  if number > guess:
+    print("Too low")
+    guess = int(input("Guess again"))
+print("Incorrect, the correct number was", number)
